d06dd4be2c4de0b3959ece11a238962e2b762539
Kalle <kh.hansson@gmail.com>
2026-05-07 07:11:49 +0000

    Update slayzone to 0.28.0

--- Casks/slayzone.rb
+++ Casks/slayzone.rb
@@ -1,14 +1,14 @@
 cask "slayzone" do
-  version "0.27.2"
+  version "0.28.0"
 
   on_arm do
-    sha256 "46bed2d0dbe01e1bfe75a4d3795ae0342d2008221846171f1f6930328c1211fa"
-    url "https://github.com/debuglebowski/slayzone/releases/download/v0.27.2/SlayZone-arm64.dmg"
+    sha256 "151efe2fbfd40723de0ea50312030a2c499d8091446c69f07c5d6a7b2d4128b9"
+    url "https://github.com/debuglebowski/slayzone/releases/download/v0.28.0/SlayZone-arm64.dmg"
   end
 
   on_intel do
-    sha256 "a325d8651687df33b48870f1b06ee6bdb3d6dd25ccf2f48ab3fdc6c698aec7aa"
-    url "https://github.com/debuglebowski/slayzone/releases/download/v0.27.2/SlayZone-x64.dmg"
+    sha256 "b288e81e346b0cdfc00d30c2515a5c7a5da39ea2ce76649576626f7a9994923b"
+    url "https://github.com/debuglebowski/slayzone/releases/download/v0.28.0/SlayZone-x64.dmg"
   end
 
   name "SlayZone"
